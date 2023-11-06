97fbb7c248e6e24728560cc6ea419c9f34cf98df
fred <domaine75@gmail.com>
2023-11-06 23:15:43 +0100

    clean code

---
+++ MusicPlayerApp/FormMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MusicPlayerApp
+{
+  public partial class MusicPlayerApp : Form
+  {
+    public MusicPlayerApp()
+    {
+      InitializeComponent();
+    }
+
+    //Create Global Variables of String Type Array to save the titles or name of the Tracks and path of the track
+    string[] paths, files;
+
+    private void BtnSelectSongs_Click(object sender, EventArgs e)
+    {
+      //Code to SElect Songs
+      OpenFileDialog ofd = new OpenFileDialog();
+      //Code to select multiple files
+      ofd.Multiselect = true;
+      if (ofd.ShowDialog() == DialogResult.OK)
+      {
+        files = ofd.SafeFileNames; //Save the names of the track in files array
+        paths = ofd.FileNames; //Save the paths of the tracks in path array
+                               //Display the music titles in listbox
+        for (int i = 0; i < files.Length; i++)
+        {
+          listBoxSongs.Items.Add(files[i]); //Display Songs in Listbox
+        }
+      }
+    }
+
+    private void ListBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      //Write a code to play music
+      axWindowsMediaPlayerMusic.URL = paths[listBoxSongs.SelectedIndex];
+    }
+
+    private void PictureBox1_Click(object sender, EventArgs e)
+    {
+      //Code to Close the App
+      Close();
+    }
+  }
+}
